81031752f4056ad753db92a6ba7ecab2e6c2fdc9
FERNANDO GUIMARÃES SILVA <162770609+Fernandofgs91@users.noreply.github.com>
2026-04-16 14:44:43 -0300

    Update Header.jsx

--- lista05/src/components/Header.jsx
+++ lista05/src/components/Header.jsx
@@ -3,10 +3,9 @@ import "../styles/header.css";
 export default function Header() {
   return (
     <header className="header">
-      {/* <p>MINISTÉRIO DA EDUCAÇÃO</p>
-      <p>SECRETARIA DE EDUCAÇÃO PROFISSIONAL E TECNOLÓGICA</p>
-      <h2>INSTITUTO FEDERAL DE EDUCAÇÃO, CIÊNCIA E TECNOLOGIA</h2>
-      <h3>TRIÂNGULO MINEIRO – Campus Uberlândia Centro</h3> */}
+    <span>
+                <h1>Página do Primeiro React</h1>
+            </span>
     </header>
   );
 }
