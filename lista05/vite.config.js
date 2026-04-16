64ff7c32e5307be91ba2477232e5c69abe6d6e61
FERNANDO GUIMARÃES SILVA <162770609+Fernandofgs91@users.noreply.github.com>
2026-04-16 19:24:34 -0300

    Update vite.config.js

--- lista05/vite.config.js
+++ lista05/vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // Certifique-se de que o repositório no GitHub se chama 'react' e a pasta 'lista05'
-  // Se o link for fernandofgs91.github.io/lista05/, mude para '/lista05/'
-  base:'/lista05/'
+  // Comente ou remova a linha abaixo durante o desenvolvimento local (npm run dev)
+  // base: '/lista05/' 
 })
