cf268b851391b9d685bef508507b225750ba690b
FERNANDO GUIMARÃES SILVA <162770609+Fernandofgs91@users.noreply.github.com>
2026-04-16 17:16:48 -0300

    Update App.jsx

--- lista05/src/App.jsx
+++ lista05/src/App.jsx
@@ -1,7 +1,7 @@
-import Header from "./components/Header.jsx";
-import AcessoInfo from "./components/AcessoInfo.jsx";
-import GridInfo from "./components/GridInfo.jsx";
-import Gridcolo from "./components/Gridcolo.jsx";
+import Header from "../components/Header.jsx";
+import AcessoInfo from "../components/AcessoInfo.jsx";
+import GridInfo from "../components/GridInfo.jsx";
+import Gridcolo from "../components/Gridcolo.jsx";
 
 function App() {
   return (
