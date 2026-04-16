16b6be74d01dbd3e73b57daafe9ca2212b2248fd
FERNANDO GUIMARÃES SILVA <162770609+Fernandofgs91@users.noreply.github.com>
2026-04-16 16:20:57 -0300

    Update main.jsx

--- lista05/src/main.jsx
+++ lista05/src/main.jsx
@@ -2,11 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx"; // Garanta que o arquivo App.jsx existe nesta pasta
 import "./styles/global.css"; // Garanta que este caminho está correto
-import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(
-  <BrowserRouter basename="/react">
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
     <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+  </StrictMode>
 );
