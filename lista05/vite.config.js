ace6469f202026754199aef4d1a91d95e46dff23
FERNANDO GUIMARÃES SILVA <162770609+Fernandofgs91@users.noreply.github.com>
2026-04-16 20:10:15 -0300

    Update vite.config.js

--- lista05/vite.config.js
+++ lista05/vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/lista05', 
   plugins: [react()],
   // Comente ou remova a linha abaixo durante o desenvolvimento local (npm run dev)
-  // base: '/lista05/' 
+ 
 })
