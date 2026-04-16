8c82c8ccfbf192cb727d17c0f7014f80613fd72b
FERNANDO GUIMARÃES SILVA <162770609+Fernandofgs91@users.noreply.github.com>
2026-04-16 15:51:12 -0300

    Update main.jsx

--- lista05/src/main.jsx
+++ lista05/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import "./styles/global.css";
+import App from "./App.jsx"; // Garanta que o arquivo App.jsx existe nesta pasta
+import "./styles/global.css"; // Garanta que este caminho está correto
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
